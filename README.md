7477fa3ff1fcd2a5b71891eb9d270ff402fa02fc
James <james@gibbard.me>
2023-08-20 22:35:42 +0100

    Allow setting viewport and image size from the command line

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # Map Generator
 Generates a world map from a shapefile of country boundaries.
 
-![image info](./example.png)
-
 This application is mainly a demo of three header only libraries.
 
 1. `shapefile.hpp`: A library for reading ESRI [Shapefiles](https://www.esri.com/content/dam/esrisites/sitecore-archive/Files/Pdfs/library/whitepapers/pdfs/shapefile.pdf).
@@ -16,22 +14,49 @@ make
 This will build the software in `./build/`.
 
 ## Run
+### First Run Setup
 Before running the software for the first time, maps can be downloaded using:
 
 ```bash
 make maps
 ```
 
-This downloads shapefiles from: https://www.naturalearthdata.com/downloads/
+This downloads shapefiles from: https://www.naturalearthdata.com/downloads/.
 The 50m and 10m resolution maps will be stored in `./maps/`.
 
-To generate a map:
+### Basic Example
+To generate a world map:
 ```bash
 ./build/map_gen ./maps/ne_50m_admin_0_countries_lakes.shp > map_50m.bmp
 ./build/map_gen ./maps/ne_10m_admin_0_countries_lakes.shp > map_10m.bmp
 ```
+Note that the program outputs to `stdout`, and therefore the output needs to be redirected to a file or piped to another application.
 
-To pass to imagemagick for resizing or converting to another file format:
+### Usage
 ```bash
-./build/map_gen ./maps/ne_50m_admin_0_countries_lakes.shp | convert - -resize "50%" map_50m.png
+map_gen <path_to_map_shapefile> [image_width] [image_height]
+map_gen <path_to_map_shapefile> x_min x_max y_min y_max [image_width] [image_height]
 ```
+
+If an `image_width` is not specified, a default value of 3600px is used, and the `image_height` is automatically scaled to preserve the aspect ratio of the map.
+
+#### Example 1
+Generate a map of Australia with a width of 700px and a height set automatically to preserve map aspect ratio:
+```bash
+./build/map_gen ./maps/ne_10m_admin_0_countries_lakes.shp 111.72 157.51 -40.15 -10.25 700 > aus.bmp
+```
+![image info](./examples/example0.png)
+
+#### Example 2
+Generate a world map (3600px x 1800px) and pass to imagemagick to resize to 20% the original size and then convert to `PNG`.
+```bash
+./build/map_gen ./maps/ne_50m_admin_0_countries_lakes.shp | convert - -resize "20%" map_50m.png
+```
+![image info](./examples/example1.png)
+
+#### Example 3
+Generate a world map with a forced size of 256px x 256px.
+```bash
+./build/map_gen ./maps/ne_50m_admin_0_countries_lakes.shp  256 256 > squashed.bmp
+```
+![image info](./examples/example2.png)
--- map_gen.cpp
+++ map_gen.cpp
@@ -1,14 +1,96 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include "polygon.hpp"
 #include "image.hpp"
 #include "shapefile.hpp"
 
+void print_help() {
+    std::cerr << "\nUsage: " << std::endl;
+    std::cerr << "\t" << "map_gen <path_to_map_shapefile> [image_width] [image_height]" << std::endl;
+    std::cerr << "\t" << "map_gen <path_to_map_shapefile> ";
+    std::cerr << "x_min x_max y_min y_max [image_width] [image_height]" << std::endl;
+}
+
+template<typename T>
+T _read_arg_val(char* str) {
+    // Only allow T to be int and double types
+    // Will trigger a build error if this is not true
+    static_assert(std::is_same<T, int>::value || 
+                  std::is_same<T, double>::value);
+}
+
+template<>
+int _read_arg_val<int>(char* str) { return std::stoi(str); }
+template<>
+double _read_arg_val<double>(char* str) { return std::stod(str); }
+
+template<typename T>
+T read_arg(char* str, int min, int max, const std::string& name) {
+    T val = 0;
+    try {
+        val = _read_arg_val<T>(str);
+    } catch (std::invalid_argument& error)  {
+        std::cerr << "Error: " << error.what() << std::endl;
+        print_help();
+        exit(1);
+    }
+    if (val < min || val > max) {
+        std::cerr << "Error: " << name << " must be between ";
+        std::cerr << min << " and " << max << std::endl;
+        print_help();
+        exit(1);
+    }
+    return val;
+}
+
 int main(int argc, char **argv) {
 
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <path_to_map_shapefile>" << std::endl;
+    // Image defaults
+    const unsigned int width_default = 3600;
+    unsigned int width = width_default;
+    unsigned int height = width_default / 2.0;
+    bool maintain_aspect_ratio = true;
+    double x_min = -180.0;
+    double x_max = 180.0;
+    double y_min = -90.0;
+    double y_max = 90.0;
+
+    if (argc == 2) { // No image size specifed
+        width = width_default;
+    } else if (argc == 3) {  // Just width specified
+        width = read_arg<int>(argv[2], 1, 50000, "image_width");
+    } else if (argc == 4) {  // Force size. Aspect ratio might be wrong
+        width = read_arg<int>(argv[2], 1, 50000, "image_width");
+        height = read_arg<int>(argv[3], 1, 50000, "image_height");
+        maintain_aspect_ratio = false;
+    } else if (argc >= 6 && argc <= 8) {  // x+y min+max specified 
+        x_min = read_arg<double>(argv[2], -180.0, 180.0, "x_min");
+        x_max = read_arg<double>(argv[3], -180.0, 180.0, "x_max");
+        y_min = read_arg<double>(argv[4], -90.0, 90.0, "y_min");
+        y_max = read_arg<double>(argv[5], -90.0, 90.0, "y_max");
+        if (argc == 6) {  // No image size specifed
+            width = width_default;
+        } else if (argc == 7) {  // Just width specified
+            width = read_arg<int>(argv[6], 1, 50000, "image_width");
+        } else {  // Force size. Aspect ratio might be wrong
+            width = read_arg<int>(argv[6], 1, 50000, "image_width");
+            height = read_arg<int>(argv[7], 1, 50000, "image_height");
+            maintain_aspect_ratio = false;
+        }
+    } else {
+        print_help();
         return 1;
+    }
+
+    if ((x_min >= x_max) || (y_min >= y_max)) {
+        std::cerr << "Error: x/y_min is greater or equal to x/y_max" << std::endl;
+        print_help();
+        return 1;
+    }
+
+    if (maintain_aspect_ratio) {
+        height = std::ceil(width * ((y_max - y_min) / (x_max - x_min)));
     }
 
     // Open and parse the shapefile
@@ -24,16 +106,15 @@ int main(int argc, char **argv) {
     std::vector<Polygon> polygons;
     shapefile.get_polygons(polygons);
 
-    unsigned int width = 3600;
-    unsigned int height = width / 2;
-    double x_scale = static_cast<double>(width - 1) / 360.0;
-    double y_scale = static_cast<double>(height - 1) / 180.0;
+    double x_scale = static_cast<double>(width - 1) / (x_max - x_min);
+    double y_scale = static_cast<double>(height - 1) / (y_max - y_min);
+
+    double x_shift = -x_min;
+    double y_shift = -y_min;
 
     // Shift and scale the lat/lng polygons to match the image size
     for (auto& polygon : polygons) {
-        // X shift: -180 to 180 -> 0 to 360
-        // Y shift:  -90 to  90 -> 0 to 180
-        polygon.shift(180.0, 90.0);
+        polygon.shift(x_shift, y_shift);
         polygon.scale(x_scale, y_scale);
     }
 
@@ -50,7 +131,7 @@ int main(int argc, char **argv) {
 
     // Draw all the country boundaries
     for (auto& polygon : polygons) {
-        image.draw_polygon(polygon, true, 1, true, 3);
+        image.draw_polygon(polygon, true, 1, true, 2);
     }
 
     // Output bitmap to stdout
